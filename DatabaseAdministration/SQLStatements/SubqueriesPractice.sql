7d1a69052c0db6b046fcd729364ac8159d881430
tendoMbambisa <tendombambisa@gmail.com>
2025-02-28 13:16:05 +0200

    added excel files

--- DatabaseAdministration/SQLStatements/SubqueriesPractice.sql
+++ DatabaseAdministration/SQLStatements/SubqueriesPractice.sql
@@ -1,3 +1,20 @@
 --Subqueries and aggregation questions
 --Find the most expensive product from the production.products table.
-select 
+select product_name,list_price 
+from production.products
+where list_price = (select max(list_price) 
+as highest_price from production.products);
+
+--Identify customers who have spent more than $1000 across all their orders.
+SELECT customer_id, first_name, last_name, email
+FROM sales.customers
+WHERE customer_id IN (
+    SELECT o.customer_id
+    FROM sales.orders o
+    JOIN sales.order_items oi ON o.order_id = oi.order_id
+    GROUP BY o.customer_id
+    HAVING SUM(oi.quantity * oi.list_price * (1 - oi.discount)) > 1000
+);
+
+
+
