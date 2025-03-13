bf5677da8838afaf3d176d922865e08abcc7899d
tendoMbambisa <tendombambisa@gmail.com>
2025-03-13 09:01:24 +0200

    created script on functions testing different functions out

---
+++ DatabaseAdministration/SQLStatements/Udemy/SystemFunctions.sql
@@ -0,0 +1,23 @@
+SELECT max(rate)
+  FROM [AdventureWorks2022].[HumanResources].[EmployeePayHistory]
+
+  SELECT Min(rate)
+  FROM [AdventureWorks2022].[HumanResources].[EmployeePayHistory]
+
+    SELECT count(*)
+  FROM [AdventureWorks2022].[HumanResources].[EmployeePayHistory]
+
+    SELECT sum(rate)
+  FROM [AdventureWorks2022].[HumanResources].[EmployeePayHistory]
+
+    SELECT count(rate)
+  FROM [AdventureWorks2022].[HumanResources].[EmployeePayHistory]
+
+  print upper('dotnet');
+  print lower('dotnet');
+
+  print convert(int,15.56);
+
+  print getdate();
+
+  print year(getdate());
