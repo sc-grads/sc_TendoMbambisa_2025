3c956a49d1ed934ccc462e16ca46dde9189b5d17
tendoMbambisa <tendombambisa@gmail.com>
2025-03-17 14:24:35 +0200

    did trasaction log restore

---
+++ DatabaseAdministration/SQLStatements/Udemy/TransactionLogRestore.sql
@@ -0,0 +1,3 @@
+RESTORE LOG [AdventureWorks2022] FROM  DISK = N'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\Backup\AdventureWorks2022_tran_1.trn' WITH  FILE = 1,  NOUNLOAD,  STATS = 10
+GO
+    
